fd988286207ad0e5d55322091c80f8263f0ab0f8
jgierlach <jegierlach@gmail.com>
2020-08-02 19:49:29 -0400

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,73 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input v-model="partNumber" placeholder="Enter Part Number" />
+    <button @click="scrapeDocs">Generate Listing</button>
+    <br />
+    <h1>{{partData.title}}</h1>
+    <!-- <br /> -->
+    <div class="center">
+      <ul class="left width">
+        <li>{{partData.firstBullet}}</li>
+        <li>{{partData.secondBullet}}</li>
+        <li>{{partData.thirdBullet}}</li>
+        <li>{{partData.fourthBullet}}</li>
+      </ul>
+    </div>
+    <div class="center">
+      <p class="left width">
+        <strong>keywords:</strong>
+        {{partData.keywords}}
+      </p>
+    </div>
+    <br />
+    <img class="format-img" alt="docs image" :src="partData.img" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {},
+  data() {
+    return {
+      partData: {},
+      partNumber: '',
+    }
+  },
+  methods: {
+    async scrapeDocs() {
+      const response = await axios.get(
+        `/api/generate/ujoint/${this.partNumber}`
+      )
+      console.log('response', response)
+      this.partData = response.data
+    },
+  },
 }
 </script>
 
 <style>
+.format-img {
+  width: 1000px;
+  height: 1000px;
+}
+
+.center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.left {
+  text-align: left;
+}
+
+.width {
+  width: 500px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ srv/index.js
@@ -0,0 +1,61 @@
+import express from 'express';
+import puppeteer from 'puppeteer'
+
+export default (app, http) => {
+  app.use(express.json());
+
+  app.get('/api', (req, res) => {
+    res.json({ msg: 'service is up' });
+  });
+
+  app.get('/api/generate/ujoint/:id', async (req, res) => {
+    try {
+      const partNumber = req.params.id
+      const url = `https://www.docsautoparts.com/product/${partNumber}`
+
+      const browser = await puppeteer.launch()
+      const page = await browser.newPage()
+      await page.goto(url)
+
+      // Find the images src
+      const src = await page.evaluate(async () => {
+        const element = document.querySelector('.zoomImg').getAttribute('src')
+        return element
+      })
+
+      // Find the price
+      let price = await page.evaluate(async () => {
+        const element = document.querySelector('.price').textContent;
+        return element
+      });
+      price = price.split(" ")[1]
+
+      // Extract the part number and prefix it with the Doc's D
+      const partName = `D${partNumber.slice(2, partNumber.length)}`
+
+      // Find replaces at text
+      const replaces = await page.$$(".woocommerce-product-details__short-description")
+
+      // Format all of the part data into an object
+      const partData =
+      {
+        price: price,
+        img: src,
+        title: `Doc's Auto Parts | ${partName} | Universal Joint`,
+        firstBullet: `REPLACES - `,
+        secondBullet: `BUILT TO LAST - Our ${partName} U Joint is made with chrome-nickel alloy steel that is induction heated with hardened bearing caps and coated to resist oxidation and corrosion. You can be sure this U joint is in it for the long haul.`,
+        thirdBullet: `OEM GRADE QUALITY - We stand behind the durability and quality of every single one of our parts. Our U Joints are thoroughly tested and of OEM grade quality.`,
+        fourthBullet: `FAMILY VALUES - Docs Auto Parts is a family business and those family values extend to all our customers. If you feel our product has not lived up to your expectations in some way please reach out to us and we’ll resolve your concerns immediately!`,
+        keywords: `spicer life non greaseable universal joint snap ring outside style bearing cap diameter strength alloy steel cross torque horsepower flush lube fitting needle hardness carboxylated nitrile rubber grease seal use compressed double lip design moog`
+      }
+
+      // console.log('partData', partData)
+      await browser.close()
+
+      // Send part data payload
+      res.json(partData)
+    } catch (err) {
+      console.error(err)
+    }
+  })
+}
